b006e14069584dd68863ab477d8678f1038a971c
MWegenke <matthew.wegenke@galvanize.com>
2026-02-03 21:12:29 -0600

    implemented local storage and archive list

--- index.js
+++ index.js
@@ -1,23 +1,42 @@
-/*
-  TODO
-    -user should be able to view all tasks
-    -able to add a to-do item to the list
-    -should be able to mark a task complete
-    -remove tasks by clicking
-    -page shouldn't be ugly
-*/
-
+// Query Selectors
 let userInput = document.querySelector('#userText')
 let userSubButton = document.querySelector('#userSubBtn')
 let taskList = document.querySelector('#taskList')
+let archiveElement = document.querySelector('#doneList')
 
+// Global Variables
 let taskCounter = 1
 let tasks = []
+let archives = []
+
+// Initial Events
+document.addEventListener('load', loadFromStorage())
+userSubButton.addEventListener("click", () => grabUserInput())
+
+// Functions
+function loadFromStorage(){
+
+  JSON.parse(localStorage.getItem('archive')) == null ? archives = [] : archives = JSON.parse(localStorage.getItem('archive'))
+  JSON.parse(localStorage.getItem('list')) == null ? tasks = [] : JSON.parse(localStorage.getItem('list')).forEach(task => createTask(task))
+
+  if(archives.length > 0){
+    archiveElement.style.display = 'block'
+    archives.forEach(task => {
+      let newElm = document.createElement('div')
+      newElm.innerText = task
+      newElm.className = 'complete'
+      console.log(newElm.innerText)
+      archiveElement.appendChild(newElm)
+    })
+  }
+
+}
+
 
 function grabUserInput(){
-  tasks.push(userInput.value)
-  // clear out input box after a submit
-  createTaskDiv(userInput.value)
+  createTask(userInput.value.trim())
+
+  userInput.value=""
 }
 
 
@@ -28,27 +47,59 @@ function clickTask(id){
 
 function clickTask(id){
   let task = document.querySelector(`#${id}`)
-  task.className = "complete"
+
+  if(task.className === "complete"){
+    task.className = ''
+    task.firstChild.checked = false
+  }else{
+    task.className = 'complete'
+    task.firstChild.checked = true
+  }
+
 }
 
-function createTaskDiv(inVal){
+function removeTask(id){
+  let task = document.querySelector(`#${id}`)
+  let text = task.innerText.slice(0,-1)
+  console.log(text)
+  archives.push(text)
+
+  tasks = tasks.filter(task => task != text)
+
+  localStorage.setItem('archive',JSON.stringify(archives))
+  taskList.removeChild(task)
+  task.firstChild.remove()
+  task.lastElementChild.remove()
+  archiveElement.appendChild(task)
+
+  archiveElement.children.length > 0 ? archiveElement.style.display = 'block' : archiveElement.style.display = 'none'
+
+  localStorage.setItem('list',JSON.stringify(tasks))
+}
+
+function createTask(inVal){
 
   let curId = `task${taskCounter}`
-
   let newDiv = document.createElement('div')
+  let checkBox = document.createElement('input')
+  let taskBtn = document.createElement('button')
 
   newDiv.id = curId
+  checkBox.type = 'checkbox'
+  taskBtn.innerText = "X"
 
   let newTask = taskList.appendChild(newDiv)
-
+  newTask.appendChild(checkBox)
   newTask.append(inVal)
+  newTask.append(taskBtn)
+
+  tasks.push(inVal)
 
   newTask.addEventListener('click', () => clickTask(curId))
+  taskBtn.addEventListener('click', () => removeTask(curId))
 
   taskCounter++
+
+  localStorage.setItem('list',JSON.stringify(tasks))
 }
 
-
-
-
-userSubButton.addEventListener("click", () => grabUserInput())
